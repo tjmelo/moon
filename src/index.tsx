1629c828c4e714df5609dfb7ac57981818f16d55
Thiago Melo <thiago.tmwd@gmail.com>
2025-01-14 18:22:40 -0300

    feat: module federationize

--- src/index.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { render } from "react-dom";
-import "./config/i18n";
-import App from "./App";
-
-export const Root = () => <App />;
-
-render(<Root />, document.querySelector("#app"));
