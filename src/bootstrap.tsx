083d43108cb3a5764ab808e3a4e6062d3d050125
Thiago Melo <thiago.tmwd@gmail.com>
2025-01-16 20:39:31 -0300

    feat: improving microfrontend

--- src/bootstrap.tsx
+++ src/bootstrap.tsx
@@ -3,10 +3,18 @@ import { createRoot } from "react-dom/client";
 import "./config/i18n";
 import App from "./App";
 
-const container = document.querySelector("#app");
+export const container = document.querySelector("#app");
 
 if (container) {
-  const root = createRoot(container); 
+  const shadowRoot = container.attachShadow({ mode: "open" });
+
+  const event = new CustomEvent("shadowroot-created");
+  container.dispatchEvent(event);
+
+  const shadowContainer = document.createElement("div");
+  shadowRoot.appendChild(shadowContainer);
+
+  const root = createRoot(shadowContainer);
   root.render(<App />);
 } else {
   console.error("Failed to find the app container");
